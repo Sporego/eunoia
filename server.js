2b03c2f608036bd51b381718276a72602deaed6a
Sporego <Hello@Filbert.me>
2019-06-17 21:31:06 -0400

    Create browser module skeleton

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
