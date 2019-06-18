f3eb032c616ac9c50c0aee1d9236551877861176
Sporego <Hello@Filbert.me>
2019-06-18 19:42:38 -0400

    move html to protoHTML directory

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const port = process.env.PORT || 5000;
+
+const app = express();
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
