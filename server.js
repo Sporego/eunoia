90d5901097febd342e77e9b7a33d9f1f8ffc15cb
Sporego <Hello@Filbert.me>
2019-06-20 09:31:09 -0400

    mongoose

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+//Middleware Block
+app.use(bodyParser.urlencoded({ extended: false })); // https://www.npmjs.com/package/body-parser#extended
+app.use(bodyParser.json()); //https://www.npmjs.com/package/body-parser#bodyparserjsonoptions
 
 // create a GET route
 app.get("/express_backend", (req, res) => {
   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
 });
+
+//Database Block
+mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true }); //Connect to test
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  // we're connected!
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
