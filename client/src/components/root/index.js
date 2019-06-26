e5c40d2f153a1037996b7844cc9525f44c86a144
Sporego <Hello@Filbert.me>
2019-06-25 22:25:23 -0400

    root finished

---
+++ client/src/components/root/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function Root() {
+  return (
+    <div style={{ height: 800 }}>
+      <header>
+        <img src="./img/textlogo.PNG" alt="logo" class="center" />
+      </header>
+
+      <div class="center2">
+        <a class="push" href="home.html">
+          Get Started
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Root;
