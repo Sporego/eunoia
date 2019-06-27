804cf649fa253216eb53abaf1916c251ad9fa736
Jay Choi <prayerjoshua95@gmail.com>
2019-06-27 19:30:10 -0400

    update columbia

---
+++ client/src/components/userProfile/index.js
@@ -0,0 +1,89 @@
+import React from "react";
+import "./style.css";
+
+function Root() {
+  return (
+    <div style={{ height: 800 }}>
+    <div class="wrapper">
+        <img src="propic.jpg" alt="" class="image--cover" />
+      </div>
+
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+        <div id="topNav" class="navigation">
+                <span id="closebtn" onclick="navToggle()">
+                    <span class="line1"></span>
+                    <span class="line2"></span>
+                    <span class="line3"></span>
+                </span>
+
+              <ul class="menulist">
+                <li><a class="menuitems" href="#about">HOME</a></li>  
+                <li><a class="menuitems" href="#category">GAMES</a></li>					
+                <li><a class="menuitems" href="#contact">EDIT MY PROFILE</a></li>
+                <li><a class="menuitems" href="#login">LOG OUT</a></li>
+
+              </ul>			
+              </div>
+              
+              <div id="main">
+                <header><img src="textlogo.png" id="logopic" alt="logo"></header>
+              </div>
+    
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<footer id="copyrights">_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+  <div><p>All of the copyrights of this website is belong to members of EUNOIA COMPANY.
+   <br>You are not allowed to edit this website without our permisssion.
+   <br>You may be accused once you break the policy.   
+  </p>
+  
+  </div>
+    </div>
+  );
+}
+
+export default Root;
