b557f6c052bcbc7a7749074b0b9cde39593ede94
Sporego <Hello@Filbert.me>
2019-06-29 00:10:20 -0400

    added require for path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 //load api routes
 const users = require("./routes/api/users");
